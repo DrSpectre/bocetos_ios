9693b00ceb1b4de0414076bd1fd4b777bb425d9d
Jadzia Eleonore <bluakorvo@protonmail.com>
2024-09-25 10:06:55 -0600

    [AGREGA] Un poco mas de codigo al controlador para obtener citas y manejarlas

--- boceto_1/boceto_1/ViewController.swift
+++ boceto_1/boceto_1/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     var cita_para_enviar: Cita = Cita(quien_lo_dijo: "Creeper", que_dijo: "Tssseñor")
+    var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
 
     override func viewDidLoad() {
+        citas_disponibles.generar_citas_falsas()
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view
+        
+        let valor = Int.random(in: 0...100)
+        Double.random(in: 0...100)
     }
 
     
     @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCitas? {
-        return ControladorPantallaCitas(muro_texto: "TSSSSSSSEÑOR", de_quien: "Creeper", coder: coder)
+        return ControladorPantallaCitas(cita_para_citar: cita_para_enviar, coder: coder)
     }
 }
 
